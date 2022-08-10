6c9849fb84686eaf6ebe0704cf7d4dda31950ad2
Simon Kjærulff <43952666+DukaSoft@users.noreply.github.com>
2022-08-10 08:04:24 +0200

    Add project files.

---
+++ ColorConverter/Converter.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DukaSoft.ColorConverter
+{
+	public class Converter
+	{
+		public void Convert(string Color)
+		{
+
+		}
+	}
+}
